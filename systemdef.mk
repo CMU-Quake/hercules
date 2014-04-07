83f46d2b9bb12ab239db4fc19a1aa83474b5e687
Ricardo Taborda <ricardotaborda@gmail.com>
2014-04-07 00:10:33 -0500

    new system configuration for Mac OS X Mavericks (10.9) woriking with OpenMPI

--- systemdef.mk
+++ systemdef.mk
@@ -174,3 +174,13 @@ ifeq ($(SYSTEM), MACBOOK)
         CPPFLAGS    += -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
 endif
  
+ifeq ($(SYSTEM), MAVERICKS)
+	MPI_DIR      = /opt/local/
+	MPI_INCLUDE  = $(MPI_DIR)/include/openmpi-mp/openmpi/ompi/mpi/cxx
+	CC           = $(MPI_DIR)/bin/mpicc-openmpi-mp
+	CXX          = $(MPI_DIR)/bin/mpicxx-openmpi-mp
+	LD           = $(MPI_DIR)/bin/mpicxx-openmpi-mp
+	CXXFLAGS    += -DMPICH_IGNORE_CXX_SEEK
+	CFLAGS      += -Wall
+	CPPFLAGS    += -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+endif
